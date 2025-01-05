58a1ebae5ea6dda3a63ca2fd93472a20c0a97ecb
Phil Hardegen <philhardegen@gmail.com>
2025-01-05 13:04:19 -0500

    link to github

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,8 @@ export default function Home() {
         <h2>I made a ridiculous game to test out the coding capabilities of o1 preview. It kinda sucks, but still pretty neat that it took 5 minutes of work and virtually no coding.</h2>
 
         <Link className={styles.link} href="/index.html">Play the game</Link>
+
+        <Link className={styles.link} href="https://github.com/philhd/deliman">See the source and the prompts that generated everything</Link>
       </main>
     </div>
   );
