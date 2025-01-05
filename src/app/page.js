17f30005574ac3a6d51654c9b1dd4d36e1934a20
Phil Hardegen <philhardegen@gmail.com>
2025-01-05 13:12:43 -0500

    explain the game

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,10 @@ export default function Home() {
         <h1>Deli Manager Showdown</h1>
         <h2>I made a ridiculous game to test out the coding capabilities of o1 preview. It kinda sucks, but still pretty neat that it took 5 minutes of work and virtually no coding.</h2>
 
+        <p>The game is set in a supermarket. You are the deli manager, and are being harassed by a throng of impatient shoppers. You must jump around and hurl deli meats at them.</p>
+
+        <b>Avoid contact with customers and their shopping carts at all costs!</b>
+
         <Link className={styles.link} href="/index.html">Play the game</Link>
 
         <Link className={styles.link} href="https://github.com/philhd/deliman">See the source and the prompts that generated everything</Link>
